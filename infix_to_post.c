1bcd14c33fb889b50dc467cffc6e6370e05f61a7
drb101005 <drb101005@gmail.com>
2025-05-04 04:17:56 +0530

    Create infix_to_post.c

---
+++ infix_to_post.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) { stack[++top] = c; }
+char pop() { return (top == -1) ? -1 : stack[top--]; }
+int priority(char c) {
+    if (c == '^') return 3;
+    if (c == '*' || c == '/') return 2;
+    if (c == '+' || c == '-') return 1;
+    return 0;
+}
+
+void infixToPostfix(char* infix, char* postfix) {
+    int i = 0, j = 0;
+    while (infix[i] != '\0') {
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        } else if (infix[i] == '(') {
+            push(infix[i]);
+        } else if (infix[i] == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // Remove '(' from stack
+        } else {
+            while (top != -1 && priority(stack[top]) >= priority(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+        i++;
+    }
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX] = "A*(B+C)-D/E";
+    char postfix[MAX];
+    infixToPostfix(infix, postfix);
+    printf("Infix: %s\nPostfix: %s\n", infix, postfix);
+    return 0;
+}
