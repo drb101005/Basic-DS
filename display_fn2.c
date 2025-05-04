f9a6728f5a671a71bbc9b21ad1aa494f6a37cfb5
drb101005 <drb101005@gmail.com>
2025-05-05 04:40:39 +0530

    Update display_fn2.c

--- display_fn2.c
+++ display_fn2.c
@@ -1,3 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a Node
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Function to push an element onto the stack
+void push(Node** top, int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    newNode->data = value;
+    newNode->next = *top;
+    *top = newNode;
+}
+
+// Function to pop an element from the stack
+int pop(Node** top) {
+    if (*top == NULL) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    Node* temp = *top;
+    int poppedValue = temp->data;
+    *top = (*top)->next;
+    free(temp);
+    return poppedValue;
+}
+
+// Function to display the stack
 void displayLinkedListStack(Node* top) {
     if (top == NULL) {
         printf("Stack is Empty\n");
@@ -10,4 +45,20 @@ void displayLinkedListStack(Node* top) {
         }
         printf("\n");
     }
+}
+
+// Main function to test the stack
+int main() {
+    Node* stackTop = NULL;
+
+    push(&stackTop, 10);
+    push(&stackTop, 20);
+    push(&stackTop, 30);
+
+    displayLinkedListStack(stackTop);
+
+    printf("Popped: %d\n", pop(&stackTop));
+    displayLinkedListStack(stackTop);
+
+    return 0;
 }
